0bb69d3ab226182c8d655de8aeef12fb419cf7b1
Maarkarb <32672648+Maarkarb@users.noreply.github.com>
2017-10-26 23:29:39 +0800

    Added all the tabs

--- src/pages/Upload/Upload.ts
+++ src/pages/Upload/Upload.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {ThanksPage} from "../Thanks/Thanks";
 
@@ -6,15 +6,31 @@ import {ThanksPage} from "../Thanks/Thanks";
   selector: 'page-upload',
   templateUrl: 'Upload.html'
 })
-export class UploadPage {
+export class UploadPage implements OnInit {
+
+  questions: string[] = [
+    'Which do you prefer: MacDonald or KFC? Why?',
+    'Why do you want to join SIA?',
+    'If you have one million dollars, what would you do with it?',
+    'What is your favourite food? Why?',
+    'What is your favourite colour? Why?',
+    'Of all the countries you have been to, which is your favourite? Why?',
+    'Of all the countries you have been to, which is your least favourite? Why?',
+
+  ]
+
+  question: string = '';
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  ngOnInit() {
+    let randomNumber = Math.floor(Math.random() * (this.questions.length - 1) + 1);
+    this.question = this.questions[randomNumber];
+  }
+
   gotoNextPage() {
     this.navCtrl.push(ThanksPage, {}, {animation: 'ios-transition'});
   }
-
-
 }
